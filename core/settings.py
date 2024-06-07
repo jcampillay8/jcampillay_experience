93adbb292278a89d5b03b6267bc1b7ee61d11682
jcampillay <jcampillayworks@gmail.com>
2024-06-07 16:52:55 -0400

    try two

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.environ.get('DEBUG')
-DEBUG = True
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOST_DEV')
 #ALLOWED_HOSTS = []
