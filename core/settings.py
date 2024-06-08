b4a35a7149dbdbed8d41606610e1b330b161db5d
jcampillay <jcampillayworks@gmail.com>
2024-06-08 17:59:24 -0400

    try nine

--- core/settings.py
+++ core/settings.py
@@ -1,25 +1,27 @@
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
+import json
 import environ
 
-# Initialize environment variables
-env = environ.Env(
-    DEBUG=(bool, False)  # Default to False if DEBUG is not set
-)
-
-# Read .env file
+env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+#SECRET_KEY = "django-insecure-v%ys6x8^0rpz##(dn&-l4hidtdi!ffpjh)109@(b(^cm8-j8(m"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOST_DEV')
 #ALLOWED_HOSTS = []
 
 
