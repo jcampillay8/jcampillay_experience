8fc908a9d0fc6c9f3a1f58777d4dd73ce297835e
jcampillay <jcampillayworks@gmail.com>
2024-08-06 10:40:25 -0400

    06 agosto 2

--- core/settings.py
+++ core/settings.py
@@ -6,28 +6,32 @@ from dotenv import load_dotenv
 import json
 import environ
 
-# Inicializa la carga de entorno
+load_dotenv()
+
 env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+#SECRET_KEY = "django-insecure-v%ys6x8^0rpz##(dn&-l4hidtdi!ffpjh)109@(b(^cm8-j8(m"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = os.environ.get('DEBUG')
 
-# Lista de hosts permitidos
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['0.0.0.0', '*', 'localhost', '127.0.0.1', 'web-production-299a1.up.railway.app'])
+environ.Env.read_env(os.path.join(os.path.dirname(__file__), '../../core/.env'))
 
-# Otras configuraciones
-CSRF_TRUSTED_ORIGINS = ['http://*', 'https://web-production-299a1.up.railway.app']
+# ALLOWED_HOSTS = env.list('ALLOWED_HOST_DEV')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['0.0.0.0','*','localhost', '127.0.0.1', 'web-production-299a1.up.railway.app'])
+#ALLOWED_HOSTS = []
 
+CSRF_TRUSTED_ORIGINS = ['http://*','https://web-production-299a1.up.railway.app']
 
 # Application definition
 
